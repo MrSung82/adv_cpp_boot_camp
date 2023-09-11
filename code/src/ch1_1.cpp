e2759fbe3f8edb2b2bdc6769c60bdea77e618478
mrsung82 <mrsung82@gmail.com>
2023-09-11 13:59:12 +0300

    Doc - chapter 1 section 2 h.b.written. Sample 1_1 h.b. implemented.

---
+++ code/src/ch1_1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <cstdio>
+
+void waitForAKey(const char* comment = nullptr)
+{
+    if (comment)
+    {
+        std::cout << comment << std::endl;
+    }
+    else
+    {
+        std::cout << "\nPress any key to continue" << std::endl;
+    }
+    static std::string q;
+    std::cin >> q;
+}
+
+template<typename T>
+void printValue(const T& val);
+
+template<>
+void printValue<int>(const int& val)
+{
+    printf_s("\nvalue of variable: %d", val);
+}
+
+template<>
+void printValue<float>(const float& val)
+{
+    printf_s("\nvalue of variable: %.7f", val);
+}
+
+int floatAsInt(float val)
+{
+    int* pInt = reinterpret_cast<int*>(&val);
+    return *pInt;
+}
+
+float intAsFloat(int val)
+{
+    float* pFlt = reinterpret_cast<float*>(&val);
+    return *pFlt;
+}
+
+int main()
+{
+    int intVar = 1078523331;
+    float fltVar = 3.14f;
+
+    printValue(floatAsInt(fltVar));
+    printValue(intAsFloat(intVar));
+    waitForAKey();
+    return 0;
+}
+
